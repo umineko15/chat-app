965a93ed9b68620b63143b03df733a4d2c1c0be0
umineko15 <saikidou15918@gmail.com>
2024-11-21 10:24:28 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+
   root to: "messages#index"
 end
