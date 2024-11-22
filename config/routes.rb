017054c7549739ab49224e7f88245e95b7831fc2
umineko15 <saikidou15918@gmail.com>
2024-11-22 13:51:47 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   devise_for :users
-
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_11_14_012845) do
   create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
